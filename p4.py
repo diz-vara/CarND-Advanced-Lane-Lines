82c7890a1d9109f5f35be937c67d2683dc725907
Anton (home) <diz.vara@gmail.com>
2017-04-16 18:49:57 +0300

    my first commit

---
+++ p4.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Apr 16 11:04:22 2017
+
+@author: Anton Varfolomeev
+"""
+cal_dir = './camera_cal/';
+out_dir = './output_images/'
+#%%
+runfile('calibrate.py')
+ret, mtx, dist = calibrate(cal_dir,9,6)
+
+
+#%%
+#read distorted image, display original and undistorted
+
+#this image was not used for calibration 
+# (not all rows visible)
+img = cv2.imread(cal_dir + '/calibration1.jpg')
+dst = cv2.undistort(img, mtx, dist, None, None)
+
+cv2.imwrite(out_dir + 'cal_distorted.png', img)
+cv2.imwrite(out_dir + 'cal_undistorted.png', dst)
+
